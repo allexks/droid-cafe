7ac83ba9bc80d8ec98a5d8f420ffce09e0993070
Aleksandar Ignatov <ani1802@abv.bg>
2019-12-10 14:49:18 +0200

    04.1 Fix navigation

--- app/src/main/java/com/example/droidcafe/MainActivity.kt
+++ app/src/main/java/com/example/droidcafe/MainActivity.kt
@@ -19,8 +19,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            onClickCart(view)
         }
     }
 
